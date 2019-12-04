888a53ddcfd0ea2fdbf1eae7d5423f5c1a6c94b8
Jeremy Adams <adamsj@objectcomputing.com>
2019-12-04 16:37:24 -0600

    image option
    - added image option
    - fixed name option for docker run

--- start_opendds_ros_docker.sh
+++ start_opendds_ros_docker.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 file_path="."
 container="opendds-ros2"
+image="opendds-nested:ros-eloquent"
 exec_mode="no"
-while getopts ":p:c:e" opt; do
+while getopts ":p:c:i:e" opt; do
 case ${opt} in
     p ) 
         file_path=$OPTARG
@@ -10,17 +11,20 @@ case ${opt} in
     c ) 
         container=$OPTARG
     ;;    
+    i ) 
+        image=$OPTARG
+    ;;   
     e ) 
         exec_mode="yes"
     ;;    
-    \? ) echo "options: [-p] path/to/volume/map [-c] container name [-e] connect to an existing container"
+    \? ) echo "options: [-p] path/to/volume/map [-c] container name [-i] image name [-e] connect to an existing container"
     exit
     ;;
 esac
 done
 
 if [ $exec_mode == "no" ]; then
-    docker run -it --rm -name $container -v $file_path:/opt/workspace opendds-nested:ros-eloquent bash
+    docker run -it --rm --name $container -v $file_path:/opt/workspace $image bash
 else
     docker exec -it $container bash
 fi
