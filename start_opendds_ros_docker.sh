e6e6b87683c19164d73403c71009a1037ea172ee
Jeremy Adams <adamsj@objectcomputing.com>
2019-12-04 16:08:09 -0600

    container operations
    - name the container
    - connect to a running container

--- start_opendds_ros_docker.sh
+++ start_opendds_ros_docker.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 file_path="."
-while getopts ":p:" opt; do
+container="opendds-ros2"
+exec_mode="no"
+while getopts ":p:c:e" opt; do
 case ${opt} in
     p ) 
         file_path=$OPTARG
     ;;
-    \? ) echo "options: [-p] path/to/volume/map"
+    c ) 
+        container=$OPTARG
+    ;;    
+    e ) 
+        exec_mode="yes"
+    ;;    
+    \? ) echo "options: [-p] path/to/volume/map [-c] container name [-e] connect to an existing container"
     exit
     ;;
 esac
 done
 
-docker run -it --rm -v $file_path:/opt/workspace opendds-nested:ros-eloquent bash
+if [ $exec_mode == "no" ]; then
+    docker run -it --rm -name $container -v $file_path:/opt/workspace opendds-nested:ros-eloquent bash
+else
+    docker exec -it $container bash
+fi
