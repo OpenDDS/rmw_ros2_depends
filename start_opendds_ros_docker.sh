92799e1d17b9321a5a301cc3f72d3b4b5d657c10
Jeremy Adams <adamsj@objectcomputing.com>
2019-12-05 11:25:54 -0600

    docker in the background
    - optionally starts the image in the background
    - default is to connect to a running container

--- start_opendds_ros_docker.sh
+++ start_opendds_ros_docker.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-file_path="."
+file_path=$HOME
 container="opendds-ros2"
 image="opendds-nested:ros-eloquent"
-exec_mode="no"
-while getopts ":p:c:i:e" opt; do
+start_container="no"
+while getopts ":p:c:i:s" opt; do
 case ${opt} in
     p ) 
         file_path=$OPTARG
@@ -14,17 +14,17 @@ case ${opt} in
     i ) 
         image=$OPTARG
     ;;   
-    e ) 
-        exec_mode="yes"
+    s ) 
+        start_container="yes"
     ;;    
-    \? ) echo "options: [-p] path/to/volume/map [-c] container name [-i] image name [-e] connect to an existing container"
+    \? ) echo "options: [-p] path/to/volume/map [-c] container name [-i] image name [-s] start the container then connect"
     exit
     ;;
 esac
 done
-
-if [ $exec_mode == "no" ]; then
-    docker run -it --rm --name $container -v $file_path:/opt/workspace $image bash
-else
-    docker exec -it $container bash
+echo $start_container
+if [ $start_container == "yes" ]; then
+    echo "starting "$container" "$file_path" "$image
+    docker run -d --rm --name $container -v $file_path:/opt/workspace $image bash -c "while true; do sleep 5; done"
 fi
+docker exec -it $container bash
